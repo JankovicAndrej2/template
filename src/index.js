bdc0d6778bc6062433032dedb73ea8148a475468
JankovicAndrej2 <jankovic.andrej02@gmail.com>
2024-08-13 00:32:09 +0200

    aa

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import "./styles/normalize.css";
 import "./styles/style.css";
 
-console.log("aa");
+let searchButton = document.querySelector(".button-search");
+let input = document.querySelector("input");
+let gifBox = document.querySelector(".gif-box");
+
+searchButton.addEventListener("click", () => {
+    if (input.value != "") {
+        fetch(
+            "https://api.giphy.com/v1/gifs/translate?api_key=EXMtPLlyzjz1o5j46TL5QVHnh6YUC376&s=" +
+                input.value,
+            { mode: "cors" }
+        )
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (response) {
+                gifBox.innerHTML = "";
+                let giphy = document.createElement("img");
+                giphy.src = response.data.images.original.url;
+                gifBox.appendChild(giphy);
+            });
+
+        input.value = "";
+    }
+});
